557b09068bad164bd36560d09a1cfcb6e3e5fd40
Roberto Aoki <roberto.aoki@contabilizei.com.br>
2020-08-01 21:17:32 -0300

    index e show publicos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index, :show]
   before_action :set_category, only: [:show, :update, :destroy]
 
   # GET /categories
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index, :show]
   before_action :set_post, only: [:show]
   before_action :set_post_auth, only: [:update, :destroy]
 
